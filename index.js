4a3f97a962c7ffade18c18e1c9fd721034bc4e55
Patrick Stephen <pjtnt11@PJs-MacBook-Air.hsd1.il.comcast.net>
2016-08-18 20:33:29 -0500

    Class changes

--- index.js
+++ index.js
@@ -187,7 +187,7 @@
 				break
 
 			case "add_homework_item":
-				let addHomeworkItemString = Bot.Message.text("What class are you adding homework for?").addResponseKeyboard(["Physics", "Math", "PLTW-POE (Cotie)", "PLTW-POE (Stevens)", "PLTW-DE", "English (Warczynski)", "English (Brach)", "English (Flanagan)", "English (Miserendino)", "Health (Simons)", "Cancel"])
+				let addHomeworkItemString = Bot.Message.text("What class are you adding homework for?").addResponseKeyboard(["Physics", "Math", "PLTW-POE (Cotie)", "PLTW-POE (Stevens)", "PLTW-DE", "English (Warczynski)", "English (Brach)", "English (Miserendino)", "Spanish 5-6 (Warar)", "Health (Simons)", "Cancel"])
 
 				callback(addHomeworkItemString)
 				break
